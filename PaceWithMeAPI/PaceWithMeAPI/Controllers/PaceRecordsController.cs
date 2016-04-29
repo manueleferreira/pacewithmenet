eedf2ad31e6b48168699313d6a8eeef09c651d27
manueleferreira <manueleferreira@gmail.com>
2016-04-28 23:05:29 -0300

    #1 config help page and create basic structure of paces

---
+++ PaceWithMeAPI/PaceWithMeAPI/Controllers/PaceRecordsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using PaceWithMeAPI.Models;
+
+namespace PaceWithMeAPI.Controllers
+{
+    public class PaceRecordsController : ApiController
+    {
+        PaceRecord[] paces = new PaceRecord[]
+        {
+            new PaceRecord { Id = 1, UserName = "@manueleferreira", Text = "legal #pace #run" }
+        };
+
+        /// <summary>
+        /// list all pace records
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<PaceRecord> GetAllPaceRecords()
+        {
+            return paces;
+        }
+
+        public IHttpActionResult GetPaceRecord(int id)
+        {
+            var pace = paces.FirstOrDefault((p) => p.Id == id);
+            if(pace == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pace);
+        }
+    }
+}
